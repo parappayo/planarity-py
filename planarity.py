f740de472da4e215feb84782e70203dcca7d8819
Jason Estey <esteyj@yahoo.com>
2019-11-29 16:16:06 -0800

    draw some lines too

--- planarity.py
+++ planarity.py
@@ -4,10 +4,13 @@ import sys, pygame
 from random import *
 from geometry2d import *
 
+background_colour = 0, 0, 0 # rgb 256
+screen_size = 1024, 768 # pixels
+
 def random_line():
 	result = Line()
-	result.y_intercept = randint(100, 500)
-	result.slope = randint(0, 20)
+	result.y_intercept = randint(100, 600)
+	result.slope = randint(-20, 20)
 	return result
 
 def generate_lines_intersections(line_count):
@@ -31,24 +34,32 @@ def find_points_along_line(line, intersections):
 	points_on_line = map(lambda x: x.point, intersections_on_line)
 	return list(points_on_line)
 
-def draw_intersection(surface, intersection):
+def draw_line(surface, line):
+	start_pos = (0, line.y_intercept)
+	end_pos = (screen_size[0], line.value_at_x(screen_size[0]))
+	width = 4
+	colour = 220, 220, 220
+	pygame.draw.line(surface, colour, start_pos, end_pos, width)
+
+def draw_point(surface, point):
 	inner_colour = 160, 160, 220
 	outer_colour = 220, 220, 220
 	border_thickness = 4
-	center = int(intersection.point.x), int(intersection.point.y)
+	center = int(point.x), int(point.y)
 	radius = 24
 	pygame.draw.circle(surface, inner_colour, center, radius)
 	pygame.draw.circle(surface, outer_colour, center, radius, border_thickness)
 
 def draw_frame(surface, lines, intersections):
+	for line in lines:
+		draw_line(surface, line)
+
 	for intersection in intersections:
-		draw_intersection(surface, intersection)
+		draw_point(surface, intersection.point)
 
 def game_loop(lines, intersections):
 	pygame.init()
 
-	background_colour = 0, 0, 0
-	screen_size = 1024, 768
 	screen = pygame.display.set_mode(screen_size)
 
 	while True:
