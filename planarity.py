06b7278f0cdea3e96704663b4f9b5836aa389e04
Jason Estey <esteyj@yahoo.com>
2019-12-01 15:54:15 -0800

    can now click-drag points

--- planarity.py
+++ planarity.py
@@ -5,11 +5,13 @@ from level_generator import *
 
 background_colour = 0, 0, 0 # rgb 256
 screen_size = 1024, 768 # pixels
+point_radius = 24
 
 class GameState:
 	def __init__(self, points, connections):
 		self.points = points
 		self.connections = connections
+		self.mouse_drag_target = False
 
 def arrange_in_circle(points):
 	theta = 0
@@ -18,6 +20,14 @@ def arrange_in_circle(points):
 		point.x = (math.cos(theta) + 1) * screen_size[0] / 2
 		point.y = (math.sin(theta) + 1) * screen_size[1] / 2
 		theta += theta_step
+
+def find_point_from_pos(points, pos, radius):
+	for point in points:
+		dx = point.x - pos[0]
+		dy = point.y - pos[1]
+		if dx * dx + dy * dy < radius * radius:
+			return point
+	return False
 
 def draw_line(surface, from_point, to_point):
 	start_pos = (from_point.x, from_point.y)
@@ -31,9 +41,8 @@ def draw_point(surface, point):
 	outer_colour = 220, 220, 220
 	border_thickness = 4
 	center = int(point.x), int(point.y)
-	radius = 24
-	pygame.draw.circle(surface, inner_colour, center, radius)
-	pygame.draw.circle(surface, outer_colour, center, radius, border_thickness)
+	pygame.draw.circle(surface, inner_colour, center, point_radius)
+	pygame.draw.circle(surface, outer_colour, center, point_radius, border_thickness)
 
 def draw_frame(surface, game_state):
 	surface.fill(background_colour)
@@ -54,10 +63,15 @@ def on_key_down(event, game_state):
 		sys.exit()
 
 def on_mouse_down(event, game_state):
-	print("debug: mouse down at {p[0]}, {p[1]}".format(p=event.pos))
+	game_state.mouse_drag_target = find_point_from_pos(game_state.points, event.pos, point_radius)
 
 def on_mouse_up(event, game_state):
-	print("debug: mouse up at {p[0]}, {p[1]}".format(p=event.pos))
+	game_state.mouse_drag_target = False
+
+def on_mouse_move(event, game_state):
+	target = game_state.mouse_drag_target
+	if target:
+		target.x, target.y = event.pos
 
 def handle_input_events(input_event_handlers, game_state):
 	for event in pygame.event.get():
@@ -70,7 +84,8 @@ def game_loop(game_state):
 		pygame.QUIT: on_quit,
 		pygame.KEYDOWN: on_key_down,
 		pygame.MOUSEBUTTONDOWN: on_mouse_down,
-		pygame.MOUSEBUTTONUP: on_mouse_up
+		pygame.MOUSEBUTTONUP: on_mouse_up,
+		pygame.MOUSEMOTION: on_mouse_move
 	}
 
 	pygame.init()
