bd459246ae365123b8458e9b860b4b1e7d424390
Jason Estey <esteyj@yahoo.com>
2020-02-23 11:06:16 -0800

    remove the profiling print spam

--- planarity.py
+++ planarity.py
@@ -16,11 +16,8 @@ def game_loop(game):
     while True:
         game_input.handle_events(pygame.event.get(), game)
         if game.redraw_required:
-            start = time.perf_counter()
             game_draw.draw_frame(screen, game)
             game.redraw_required = False
-            print("draw frame time: ", time.perf_counter() - start)
-        sys.stdout.flush()
         time.sleep(0.001)
 
 
