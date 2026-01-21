344a11dc02c91e7b6918f7cf1aa4288fab7a096e
Jason Estey <esteyj@yahoo.com>
2026-01-21 14:50:51 -0800

    refactoring the draw package a bit

--- game/draw.py
+++ game/draw.py
@@ -1,46 +1,21 @@
 import pygame
 
-pip_surface = False
-level_complete_text = False
 
-
-def init(game):
-    global pip_surface
-    pip_surface = create_pip(game)
-
-    global level_complete_text
-    level_complete_text = create_level_complete_text()
-
-
-def draw_line(surface, game, line_segment):
-    width = 4
-    colour = 220, 220, 220
-    if (line_segment == game.last_found_collision[0]) or (line_segment == game.last_found_collision[1]):
-        colour = 255, 0, 0
-    pygame.draw.line(surface, colour, line_segment[0].as_tuple(), line_segment[1].as_tuple(), width)
-
-
-def draw_point(surface, game, point):
-    dest_rect = pip_surface.get_rect()
-    dest_rect.center = int(point.x), int(point.y)
-    surface.blit(pip_surface, dest_rect)
-
-
-def draw_pip(surface, game, point):
+def draw_pip(surface, radius, point):
     inner_colour = 160, 160, 220
     outer_colour = 220, 220, 220
     border_thickness = 4
-    pygame.draw.circle(surface, inner_colour, point, game.point_radius)
-    pygame.draw.circle(surface, outer_colour, point, game.point_radius, border_thickness)
+    pygame.draw.circle(surface, inner_colour, point, radius)
+    pygame.draw.circle(surface, outer_colour, point, radius, border_thickness)
 
 
-def create_pip(game):
+def create_pip(radius):
     transparent_color = 0, 0, 0
-    size = int(game.point_radius * 2)
+    size = int(radius * 2)
     center = size // 2, size // 2
     surface = pygame.Surface((size, size), pygame.HWSURFACE)
     surface.set_colorkey(transparent_color)
-    draw_pip(surface, game, center)
+    draw_pip(surface, radius, center)
     return surface
 
 
@@ -53,18 +28,39 @@ def create_level_complete_text():
     return surface
 
 
-def draw_frame(surface, game):
-    surface.fill(game.background_colour)
+class Graphics:
 
-    for connection in game.connections:
-        draw_line(surface, game, connection)
+    def __init__(self, surface, game):
+        self.surface = surface
+        self.game = game
+        self.pip_surface = create_pip(game.point_radius)
+        self.level_complete_text = create_level_complete_text()
 
-    for point in game.points:
-        draw_point(surface, game, point)
+    def draw_frame(self):
+        self.surface.fill(self.game.background_colour)
 
-    if game.level_complete:
-        dest_rect = level_complete_text.get_rect()
-        dest_rect.center = (game.screen_size[0] // 2, game.screen_size[1] // 2)
-        surface.blit(level_complete_text, dest_rect)
+        for connection in self.game.connections:
+            self.draw_line(connection)
 
-    pygame.display.flip()
+        for point in self.game.points:
+            self.draw_point(point)
+
+        if self.game.level_complete:
+            dest_rect = self.level_complete_text.get_rect()
+            dest_rect.center = (self.game.screen_size[0] // 2, self.game.screen_size[1] // 2)
+            self.surface.blit(self.level_complete_text, dest_rect)
+
+        pygame.display.flip()
+
+    def draw_line(self, line_segment):
+        width = 4
+        colour = 220, 220, 220
+        if (line_segment == self.game.last_found_collision[0]) or (line_segment == self.game.last_found_collision[1]):
+            colour = 255, 0, 0
+        pygame.draw.line(self.surface, colour, line_segment[0].as_tuple(), line_segment[1].as_tuple(), width)
+
+
+    def draw_point(self, point):
+        dest_rect = self.pip_surface.get_rect()
+        dest_rect.center = int(point.x), int(point.y)
+        self.surface.blit(self.pip_surface, dest_rect)
--- planarity.py
+++ planarity.py
@@ -1,17 +1,15 @@
 import pygame, sys, time
 
 from game import *
-import game.draw as game_draw
-import game.input_quit
-import game.input_mouse_drag
 import game_state # TODO: move to package "planarity"
 
 
 def game_loop(game):
     pygame.init()
-    game_draw.init(game)
     screen = pygame.display.set_mode(game.screen_size)
     pygame.display.set_caption("Planarity")
+
+    graphics = Graphics(screen, game)
 
     input_handler = InputHandler()
     input_handler.add(QuitHandler())
@@ -20,7 +18,7 @@ def game_loop(game):
     while True:
         input_handler.handle_events(pygame.event.get(), game)
         if game.redraw_required:
-            game_draw.draw_frame(screen, game)
+            graphics.draw_frame()
             game.redraw_required = False
         time.sleep(0.001)
 
